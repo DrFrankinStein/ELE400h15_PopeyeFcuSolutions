c9194053ce320bf95f95622598e7be202cb38cec
houendji <hshlg1@yahoo.fr>
2015-04-18 19:40:13 -0400

    permutation n des colones

--- tests/clavier/clavier.ino
+++ tests/clavier/clavier.ino
@@ -1,7 +1,9 @@
+//#include <clavier.h>
 #include <Keypad.h>
+//#include "lcd.h"
 
 const byte ROWS = 4; //four rows
-const byte COLS = 3; //three columns
+const byte COLS = 4; //three columns
 char keys[ROWS][COLS] = {
   {'1','2','3','m'},
   {'4','5','6','o'},
@@ -9,18 +11,26 @@ char keys[ROWS][COLS] = {
   {'g','0','d','e'}
 };
 byte rowPins[ROWS] = {26, 27, 28, 29}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {33, 32, 31, 30}; //connect to the column pinouts of the keypad
+byte colPins[COLS] = {30, 31, 32, 33};  //connect to the column pinouts of the keypad
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 void setup(){
   Serial.begin(9600);
+  //kpSetup();
 }
 
 void loop(){
   char key = keypad.getKey();
-
+   //char key = kpGetValue();
+   
+  delay(100);
+  Serial.println(key);
+ // Serial.print("TEST");
+  
   if (key != NO_KEY){
     Serial.println(key);
   }
 }
+
+
