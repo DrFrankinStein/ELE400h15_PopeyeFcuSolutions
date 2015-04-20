17921a87eb0c2c75d5ffe748e752c231b29b310c
Francis Bergin <bergin.francis@gmail.com>
2015-04-20 12:22:38 -0400

    modified clavier test to print on display

--- tests/clavier/clavier.ino
+++ tests/clavier/clavier.ino
@@ -5,14 +5,18 @@
 void setup()
 {
     Serial.begin(9600);
+    lcdInit();
 }
 
 void loop()
 {
     char key = kpGetValue();
+    const char *cckey = &key;
 
     if (key != NO_KEY){
         Serial.println(key);
+        lcdClrScr();
+        lcdWriteStringAtPosition(0, 0, cckey);
     }
 
     delay(100);
